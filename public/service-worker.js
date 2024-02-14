a4fa1b5f899d913a2f53225166775ac8637a1a63
Xolani <33022688+X-olani@users.noreply.github.com>
2024-02-14 13:30:20 +0200

    file path cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,10 +5,10 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        '/',
-        './index.html',
-        './manifest.json',
-        './DFicon.png',
+        '/dopeaffirmations/',
+        '/dopeaffirmations/index.html',
+        '/dopeaffirmations/manifest.json',
+        '/dopeaffirmations/DFicon.png',
         // Add other static assets you want to cache
       ]);
     })
